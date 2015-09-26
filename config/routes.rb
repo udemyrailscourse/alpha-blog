33c071afe0a30bfd9bb542734b18646f326cd2bb
Rails Udemy Course <railsudemycourse@gmail.com>
2015-09-26 21:16:10 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
