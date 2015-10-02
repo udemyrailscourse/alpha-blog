39987b9496b7cc4e5097a4d90891d42fccb03626
Rails Udemy Course <railsudemycourse@gmail.com>
2015-10-02 20:00:46 +0000

    Add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    debugger
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
