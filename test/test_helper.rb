d64ac12151a0ba94ba686ab3f236c41b82a3e819
Rails Udemy Course <railsudemycourse@gmail.com>
2015-10-08 16:27:11 +0000

    Fix tests and update navbar with categories

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in_as(user, password)
+    post login_path, session: {email: user.email, password: password}
+  end
 end
