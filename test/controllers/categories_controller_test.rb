ea721f32d078df087e2f22c4f3753b26df2ee9d8
Rails Udemy Course <railsudemycourse@gmail.com>
2015-10-08 15:59:37 +0000

    Add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
   
   test "should get categories index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -18,5 +20,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
   end
 end
