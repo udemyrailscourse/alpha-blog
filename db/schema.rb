60edea637de4dddf11648ba82aa9109eeddae7a2
Swaroop Sagar <swaroopsagar@Samples-MacBook-Pro.local>
2016-05-07 11:53:34 +0530

    updated mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506123514) do
+ActiveRecord::Schema.define(version: 20160505123514) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id",  limit: 4
@@ -30,11 +30,6 @@ ActiveRecord::Schema.define(version: 20160506123514) do
     t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string  "image_path", limit: 255
-    t.integer "user",       limit: 4
   end
 
   create_table "users", force: :cascade do |t|
